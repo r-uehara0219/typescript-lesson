8801faf60ada1f65e05d718813825483cc87e1ea
haragumi <uehara1902@gmail.com>
2022-03-12 16:20:07 +0900

    first commit

---
+++ 2-3.ts
@@ -0,0 +1,22 @@
+declare function addEventListener(
+  arg1: string,
+  arg2: () => {},
+  arg3?: boolean | {}
+): void;
+
+// 使用例
+addEventListener("foobar", () => {});
+addEventListener("event", () => {}, true);
+addEventListener("event2", () => {}, {});
+addEventListener("event3", () => {}, {
+  capture: true,
+  once: false,
+});
+
+// エラー例
+// addEventListener("foobar", () => {}, "string");
+// addEventListener("hoge", () => {}, {
+//   capture: true,
+//   once: false,
+//   excess: true,
+// });
